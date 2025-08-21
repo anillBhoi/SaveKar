0b143efafe2b75e68cfd3cc9ef5298a5c92f11dd
Anilb08 <anilbhoi7740@gmail.com>
2025-08-21 18:51:58 +0530

    first commit

--- app/api/auth/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth"; // Make sure your authOptions are correctly imported
-
-// The new NextAuth.js App Router route handler is a single line.
-const handler = NextAuth(authOptions);
-
-// This handler will automatically manage the GET and POST requests for NextAuth.js.
-// You simply export the handler.
-export { handler as GET, handler as POST };
