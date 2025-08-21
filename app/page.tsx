9eecf7ed6fffb7151223851688cf2ca7d781d6d1
Your Name <your.email@example.com>
2025-08-21 13:34:24 +0530

    comment eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+// /* eslint-disable react-hooks/exhaustive-deps */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { useSession } from "next-auth/react";
